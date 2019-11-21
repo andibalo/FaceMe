77d1cdadd9e3b62986cab221cc6ddcf071a43f2b
andi balo <andibalo213@gmail.com>
2019-11-22 03:42:22 +0700

    Updates

---
+++ precache-manifest.9ecd50525617f9ad4177cc65603cf5b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca02932a9e5b1c0b32c2f1a716ccb123",
+    "url": "/FaceMe/index.html"
+  },
+  {
+    "revision": "b89b4b53fe92f8c78ff3",
+    "url": "/FaceMe/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "54a8adc346955d284c6e",
+    "url": "/FaceMe/static/css/main.7081f94e.chunk.css"
+  },
+  {
+    "revision": "b89b4b53fe92f8c78ff3",
+    "url": "/FaceMe/static/js/2.4150d3c6.chunk.js"
+  },
+  {
+    "revision": "54a8adc346955d284c6e",
+    "url": "/FaceMe/static/js/main.a9bcce95.chunk.js"
+  },
+  {
+    "revision": "fe3586c141563c5486d5",
+    "url": "/FaceMe/static/js/runtime-main.b491ef11.js"
+  },
+  {
+    "revision": "217c0af596911b2c9df7ad5fb7340c4d",
+    "url": "/FaceMe/static/media/Elianto-Regular.217c0af5.ttf"
+  }
+]);
